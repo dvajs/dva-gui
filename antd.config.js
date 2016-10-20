a4dfe69b98b32c3f7f7cac960c1bbc391f53cd13
fanzheng.fz <fanzheng.fz@alibaba-inc.com>
2016-10-20 14:44:14 +0800

    style: override antd styles

--- antd.config.js
+++ antd.config.js
@@ -2,9 +2,32 @@
 
 module.exports = function getConfig() {
   return {
+    // Color
+    'normal-color': '#fff',
+    // 'primary-color': '#ef8d22',
+
+    // ------ Base & Require ------
     'body-background': '#0b1d35',
     'font-family': 'Menlo, Monaco, "Courier New", monospace;',
-    'normal-color': '#e5e5e5',
-    'primary-color': '#ef8d22',
+    'text-color': '#fff',
+    'border-radius-base': 0,
+    'border-radius-sm': 0,
+
+    // Border color
+    'border-color-base': '#325d7d',        // base border outline a component
+    'border-color-split': '#325d7d',        // split border inside a component
+
+    // Input
+    // ---
+    'input-bg': 'rgba(12, 126, 187, 0.2)',
+
+    // Tooltip
+    // ---
+    'tooltip-color': '#fff',
+    'tooltip-bg': 'rgb(12, 126, 187)',
+
+    // Popover
+    // ---
+    'popover-bg': 'rgb(12, 126, 187)',
   };
 };
