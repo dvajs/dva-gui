91a760b8fd2a63482753522270958f9859037f57
绯一 <jaredlee.ljj@alipay.com>
2016-10-21 15:01:07 +0800

    fix: build script and environment

--- webpack.config.main.js
+++ webpack.config.main.js
@@ -3,12 +3,12 @@ const webpack = require('atool-build/lib/webpack');
 module.exports = function (webpackConfig) {
   webpackConfig.plugins.push(new webpack.DefinePlugin({
     'process.env': {
-      env: JSON.stringify(process.env.env),
+      env: JSON.stringify(process.env.env || 'dev'),
     },
   }));
   webpackConfig.module.loaders = webpackConfig.module.loaders.map(l => {
     if (l.exclude && l.exclude.toString().indexOf('node_modules') > -1) {
-      l.exclude = /node_modules\/(?:jscodeshift)/;
+      l.exclude = /node_modules(?:(\/jscodeshift\/dist))/;
     }
     return l;
   });
